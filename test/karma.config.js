9a8f7d1dbe1ffd3bcde74d42e11e4fb9af33e2bf
Jamie Crisman <Jamie@longest.voyage>
2015-12-20 17:24:18 -0600

    someday I will figure this out

--- test/karma.config.js
+++ test/karma.config.js
@@ -23,50 +23,50 @@ function local() {
   });
 }
 
-function sauceLabs() {
-  var customLaunchers = {
-    sl_chrome: {
-      base: "SauceLabs",
-      browserName: "chrome"
-    },
-    sl_firefox: {
-      base: "SauceLabs",
-      browserName: "firefox"
-    },
-    sl_safari: {
-      base: "SauceLabs",
-      browserName: "safari",
-      platform: "OS X 10.10"
-    },
-    sl_iphone_ios_8: {
-      base: "SauceLabs",
-      browserName: "iphone",
-      version: "8.4",
-      deviceName: "iPhone 6 Plus",
-      deviceOrientation: "portrait"
-    },
-    sl_ie_11: {
-      base: "SauceLabs",
-      browserName: "internet explorer",
-      platform: "Windows 8.1",
-      version: "11"
-    }
-  };
+// function sauceLabs() {
+//   var customLaunchers = {
+//     sl_chrome: {
+//       base: "SauceLabs",
+//       browserName: "chrome"
+//     },
+//     sl_firefox: {
+//       base: "SauceLabs",
+//       browserName: "firefox"
+//     },
+//     sl_safari: {
+//       base: "SauceLabs",
+//       browserName: "safari",
+//       platform: "OS X 10.10"
+//     },
+//     sl_iphone_ios_8: {
+//       base: "SauceLabs",
+//       browserName: "iphone",
+//       version: "8.4",
+//       deviceName: "iPhone 6 Plus",
+//       deviceOrientation: "portrait"
+//     },
+//     sl_ie_11: {
+//       base: "SauceLabs",
+//       browserName: "internet explorer",
+//       platform: "Windows 8.1",
+//       version: "11"
+//     }
+//   };
 
-  return _.extend(base(), {
-    sauceLabs: {
-      testName: "Konbini Tests"
-    },
-    browserDisconnectTimeout: 10000,
-    browserDisconnectTolerance: 1,
-    browserNoActivityTimeout: 4 * 60 * 1000,
-    captureTimeout: 4 * 60 * 1000,
-    customLaunchers: customLaunchers,
-    browsers: Object.keys(customLaunchers),
-    reporters: ["dots", "saucelabs"],
-    singleRun: true
-  });
-}
+//   return _.extend(base(), {
+//     sauceLabs: {
+//       testName: "Konbini Tests"
+//     },
+//     browserDisconnectTimeout: 10000,
+//     browserDisconnectTolerance: 1,
+//     browserNoActivityTimeout: 4 * 60 * 1000,
+//     captureTimeout: 4 * 60 * 1000,
+//     customLaunchers: customLaunchers,
+//     browsers: Object.keys(customLaunchers),
+//     reporters: ["dots", "saucelabs"],
+//     singleRun: true
+//   });
+// }
 
 module.exports = function (config) {
   //if (process.env.CONTINUOUS_INTEGRATION) {
